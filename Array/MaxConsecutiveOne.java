7276bd354066b700a962f162938f2ece5cf61f0a
krishnaSarkar5 <krishnasarkar95@gmail.com>
2023-04-12 22:28:19 +0530

    consecutive ons

---
+++ Array/MaxConsecutiveOne.java
@@ -0,0 +1,27 @@
+public class MaxConsecutiveOne {
+
+    public static int findMaxConsecutive(int [] arr){
+
+        int count = 0,maxCount=0;
+        for(int i=0;i<arr.length;i++){
+
+            if(arr[i]==1){
+                count++;
+            }else if(arr[i]==0){
+                if(count>maxCount){
+                    maxCount=count;
+                    count=0;
+                }
+                    
+            }
+        }
+        
+        return maxCount>count?maxCount:count;
+    }
+
+    public static void main(String[] args) {
+        int[] arr ={0,0,0};
+
+        System.out.println(findMaxConsecutive(arr));
+    }
+}
